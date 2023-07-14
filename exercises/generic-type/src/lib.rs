c2bcb7a06894a7e273b9d1b95dc0ee5a3e3f2fcb
Le Khac Thanh Tung <lekhacthanhtung.it@gmail.com>
2023-07-14 17:30:14 +0700

    add solution for generic-type exercies

--- exercises/generic-type/src/lib.rs
+++ exercises/generic-type/src/lib.rs
@@ -1,19 +1,23 @@
 // Exercise 1 
-// Implement struct Point to make it work.
+// Implement struct Position to make it work.
 // Make it compile
 fn exercise1() {
     let integer = Position { x: 5, y: 10 };
     let float = Position { x: 1.0, y: 4.0 };
 }
 
+struct Position<T> {
+    x: T,
+    y: T
+}
 
 
 // Exercise 2
 // Modify this struct to make the code work
 // Make it compile
-struct Point<T> {
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
 }
 
 fn exercise2() {
@@ -26,12 +30,12 @@ fn exercise2() {
 // Exercise 3
 // Make it compile
 // Add generic for Val to make the code work, DON'T modify the code in `main`.
-struct Val {
-    val: f64,
+struct Val<T> {
+    val: T,
 }
 
-impl Val {
-    fn value(&self) -> &f64 {
+impl<T> Val<T> {
+    fn value(&self) -> &T {
         &self.val
     }
 }
@@ -49,23 +53,28 @@ fn exercise3() {
 // Implementing logic
 // Run tests
 
-fn find_max<T>(collection: &[T]) -> Option<&T> {
-    todo!()
+fn find_max<T: std::cmp::Ord>(collection: &[T]) -> Option<&T> {
+    collection.iter().max()
 }
 
 // Exercise 5 
 // Reverse the elements in a collection
 // Make it compile 
 // Run tests 
-fn reverse_collection<T>(collection: &[T]) {
-    todo!()
+fn reverse_collection<T>(collection: &mut[T]) {
+    collection.reverse()
 }
 
 
 // Exercise 6
 // Function to check if a collection contains a specific value
-fn contains_value<T>(collection: &[T], value: &T) -> bool {
-    todo!()
+fn contains_value<T: std::cmp::PartialEq>(collection: &[T], value: &T) -> bool {
+    for element in collection.into_iter() {
+        if *element == *value {
+            return true;
+        }
+    }
+    false
 }
 
 // Unit tests
